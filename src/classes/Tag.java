498136dd60f270e4b4845ac8bb875f6665824c7c
BrunaSuelen <subr.garcia@gmail.com>
2021-06-08 20:50:28 -0300

    Adiciona class Task e Tag

---
+++ src/classes/Tag.java
@@ -0,0 +1,30 @@
+public class Tag {
+  private int tag_id;
+  private String name;
+  private String color;
+
+  public Tag(String name, String color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public int getId() {
+    return tag_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getColor() {
+    return color;
+  }
+}
---
+++ src/classes/Task.java
@@ -0,0 +1,66 @@
+import java.time.LocalDateTime;
+
+public class Task {
+  private int task_id;
+  private String title;
+  private String description;
+  private LocalDateTime final_date;
+  private boolean complete;
+  private int tag_id;
+
+  public Task(String title, String description, LocalDateTime final_date, boolean complete, int tag_id) {
+    this.title = title;
+    this.description = description;
+    this.final_date = final_date;
+    this.complete = complete;
+    this.tag_id = tag_id;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
+  public void setFinalDate(LocalDateTime final_date) {
+    this.final_date = final_date;
+  }
+  
+  public void setComplete(boolean complete) {
+    this.complete = complete;
+  }
+  
+  public void setTagId(int tag) {
+    this.tag = tag;
+  }
+
+  public int getId() {
+    return task_id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public LocalDateTime getFinalDate() {
+    return final_date;
+  }
+
+  public boolean geComplete() {
+    return complete;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public int getTagId() {
+    return tag_id;
+  }
+}
