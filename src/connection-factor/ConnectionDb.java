c47b5ea78a8098b606ac9a50390eabe321492085
BrunaSuelen <subr.garcia@gmail.com>
2021-06-08 20:10:21 -0300

    Conecta projeto com banco

---
+++ src/connection-factor/ConnectionDb.java
@@ -0,0 +1,20 @@
+package ConnectionFactory;
+
+import java.sql.*;
+
+public class ConnectionDb {
+  private static final String USERNAME = "root";
+  private static final String PASSWORD = "123123";
+  private static final String DATABASE_URL = "jdbc:mysql://localhost:3336/scheduledb";
+
+  public static Connection createConnection() throws Exception {
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+    return connection;
+  }
+
+  public static void main(String[] args) throws Exception {
+    Connection con = createConnection();
+    if (con != null) con.close();
+  }
+}
