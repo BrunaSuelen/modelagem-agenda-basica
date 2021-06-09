da67a09f58b8acfcb0339f68d8b3e3460dc7e636
BrunaSuelen <subr.garcia@gmail.com>
2021-06-09 18:54:37 -0300

    Adiciona menu e botçao para criar nova atividade

--- src/GUI/TimeLineGUI.java
+++ src/GUI/TimeLineGUI.java
@@ -11,17 +11,21 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import javax.swing.border.EmptyBorder;
 
 public class TimeLineGUI extends JFrame {
   static TaskDAO dp = new TaskDAO();
+  JButton addTask = new JButton("+");
   
   public TimeLineGUI() {
     setTitle("Linha do Tempo");
-    setSize(600, 500);
+    setSize(550, 450);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
     setResizable(false);
+    Menu();
     taskList();
+    addTask();
     setVisible(true);
   }
   
@@ -37,7 +41,6 @@ public class TimeLineGUI extends JFrame {
       Tag tag = task.getTag();
       
       JPanel panel = new JPanel();  
-      panel.setBounds( 40 , 80 , 200 , 200 );    
       panel.setBackground(Color.white);  
 
       JLabel label_title = new JLabel(task.getTitle());
@@ -56,4 +59,27 @@ public class TimeLineGUI extends JFrame {
       add(panel);
     }
   }
+  
+  public void addTask() {
+    JPanel gui = new JPanel(new GridLayout(2,0,5,5));
+    gui.setBorder(new EmptyBorder(10,10,10,10));
+    gui.setBackground(Color.RED);
+    gui.add(addTask);
+  }
+  
+    public void Menu() {
+        JMenuBar menuBar = new JMenuBar();  
+        JMenu item_timeline, item_tags ,item_exit;
+        item_timeline = new JMenu("Item 1");  
+        item_tags = new JMenu("Item 2");  
+        item_exit = new JMenu("Item 3");
+
+        menuBar.add(item_timeline);
+        menuBar.add(item_tags);
+        menuBar.add(item_exit);
+
+        setJMenuBar(menuBar);
+        setSize(400,400);
+        setLayout(null);
+    }
 }
