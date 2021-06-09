1bde29fc84ad2e36e40a54306de89b8d545619e6
BrunaSuelen <subr.garcia@gmail.com>
2021-06-09 14:40:40 -0300

    Corrige porta setada na conexão

--- src/connectionFactory/ConnectionDb.java
+++ src/connectionFactory/ConnectionDb.java
@@ -1,11 +1,12 @@
-package ConnectionFactory;
+package connectionFactory;
 
 import java.sql.*;
 
 public class ConnectionDb {
   private static final String USERNAME = "root";
   private static final String PASSWORD = "123123";
-  private static final String DATABASE_URL = "jdbc:mysql://localhost:3336/scheduledb";
+  private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/scheduledb";
+  
 
   public static Connection createConnection() throws Exception {
     Class.forName("com.mysql.cj.jdbc.Driver");
